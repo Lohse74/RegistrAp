4b12a84f16281c82fcf02f211616ceb7f8008db6
Lohse74 <80917962+Lohse74@users.noreply.github.com>
2021-11-12 00:18:50 -0300

    componentes

--- src/app/pages/inicioo/inicioo-routing.module.ts
+++ src/app/pages/inicioo/inicioo-routing.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ComponenteDosComponent } from 'src/app/components/componente-dos/componente-dos.component';
+import { ComponenteUnoComponent } from 'src/app/components/componente-uno/componente-uno.component';
 
 import { IniciooPage } from './inicioo.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: IniciooPage
+    component: IniciooPage,
+    //declarar las rutas hijas que se cargarán en la page "inicio"
+    children:[
+      {
+        path:'uno',
+        component: ComponenteUnoComponent
+      },
+      {
+        path:'dos',
+        component: ComponenteDosComponent
+      }
+    ]
   }
 ];
 
